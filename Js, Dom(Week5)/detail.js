c8de957d711285b26058d634bd5dafdb79e93c09
Seohwann <kksh3549@gmail.com>
2025-03-31 00:40:44 +0900

    Add files via upload

---
+++ Js, Dom(Week5)/detail.js
@@ -0,0 +1,177 @@
+const pokemon = JSON.parse(localStorage.getItem('selected'));
+const main = document.createElement('main');
+
+const detailcontainer = document.getElementById('pokemon-detail');
+detailcontainer.className = 'container';
+
+const circle = document.getElementById('circle');
+circle.className = 'circle-border';
+
+const table = document.getElementById('table');
+table.className = 'table';
+if (pokemon) {
+    const img = document.createElement('img');
+    img.src = pokemon.img;
+    img.className = 'imgsize';
+
+    circle.appendChild(img);
+    detailcontainer.appendChild(circle);
+
+    const idtr = document.createElement('tr');
+    const idtdl = document.createElement('td');
+    const idtdr = document.createElement('td');
+    idtdl.innerText = "Id";
+    idtdl.className = "table-td left";
+    idtdr.innerText = pokemon.id;
+    idtdr.className = "table-td right";
+    idtr.className = "table-tr";
+    idtr.appendChild(idtdl);
+    idtr.appendChild(idtdr);
+    table.appendChild(idtr);
+
+    const nametr = document.createElement('tr');
+    const nametdl = document.createElement('td');
+    const nametdr = document.createElement('td');
+    nametdl.innerText = "Name";
+    nametdl.className = "table-td left";
+    nametdr.innerText = pokemon.name;
+    nametdr.className = "table-td right";
+    nametr.className = "table-tr";
+    nametr.appendChild(nametdl);
+    nametr.appendChild(nametdr);
+    table.appendChild(nametr);
+    
+    const heighttr = document.createElement('tr');
+    const heighttdl = document.createElement('td');
+    const heighttdr = document.createElement('td');
+    heighttdl.innerText = "Height";
+    heighttdl.className = "table-td left";
+    heighttdr.innerText = pokemon.height;
+    heighttdr.className = "table-td right";
+    heighttr.className = "table-tr";
+    heighttr.appendChild(heighttdl);
+    heighttr.appendChild(heighttdr);
+    table.appendChild(heighttr);
+    
+    const weighttr = document.createElement('tr');
+    const weighttdl = document.createElement('td');
+    const weighttdr = document.createElement('td');
+    weighttdl.innerText = "Weight";
+    weighttdl.className = "table-td left";
+    weighttdr.innerText = pokemon.weight;
+    weighttdr.className = "table-td right";
+    weighttr.className = "table-tr";
+    weighttr.appendChild(weighttdl);
+    weighttr.appendChild(weighttdr);
+    table.appendChild(weighttr);
+
+    const typestr = document.createElement('tr');
+    const typestdl = document.createElement('td');
+    const typestdr = document.createElement('td');
+    typestdl.innerText = "Types";
+    typestdl.className = "table-td left";
+    typestdr.innerText = pokemon.types;
+    typestdr.className = "table-td right";
+    typestr.className = "table-tr";
+    typestr.appendChild(typestdl);
+    typestr.appendChild(typestdr);
+    table.appendChild(typestr);
+
+    const base_Experiencetr = document.createElement('tr');
+    const base_Experiencetdl = document.createElement('td');
+    const base_Experiencetdr = document.createElement('td');
+    base_Experiencetdl.innerText = "Base-Experience";
+    base_Experiencetdl.className = "table-td left";
+    base_Experiencetdr.innerText = pokemon['base-Experience'];
+    base_Experiencetdr.className = "table-td right";
+    base_Experiencetr.className = "table-tr";
+    base_Experiencetr.appendChild(base_Experiencetdl);
+    base_Experiencetr.appendChild(base_Experiencetdr);
+    table.appendChild(base_Experiencetr);
+
+    const abilitiestr = document.createElement('tr');
+    const abilitiestdl = document.createElement('td');
+    const abilitiestdr = document.createElement('td');
+    abilitiestdl.innerText = "Abilities";
+    abilitiestdl.className = "table-td left";
+    abilitiestdr.innerText = pokemon.abilities;
+    abilitiestdr.className = "table-td right";
+    abilitiestr.className = "table-tr";
+    abilitiestr.appendChild(abilitiestdl);
+    abilitiestr.appendChild(abilitiestdr);
+    table.appendChild(abilitiestr);
+
+    const hptr = document.createElement('tr');
+    const hptdl = document.createElement('td');
+    const hptdr = document.createElement('td');
+    hptdl.innerText = "Hp";
+    hptdl.className = "table-td left";
+    hptdr.innerText = pokemon.hp;
+    hptdr.className = "table-td right";
+    hptr.className = "table-tr";
+    hptr.appendChild(hptdl);
+    hptr.appendChild(hptdr);
+    table.appendChild(hptr);
+
+    const attacktr = document.createElement('tr');
+    const attacktdl = document.createElement('td');
+    const attacktdr = document.createElement('td');
+    attacktdl.innerText = "Attack";
+    attacktdl.className = "table-td left";
+    attacktdr.innerText = pokemon.attack;
+    attacktdr.className = "table-td right";
+    attacktr.className = "table-tr";
+    attacktr.appendChild(attacktdl);
+    attacktr.appendChild(attacktdr);
+    table.appendChild(attacktr);
+    
+    const defensetr = document.createElement('tr');
+    const defensetdl = document.createElement('td');
+    const defensetdr = document.createElement('td');
+    defensetdl.innerText = "Defense";
+    defensetdl.className = "table-td left";
+    defensetdr.innerText = pokemon.defense;
+    defensetdr.className = "table-td right";
+    defensetr.className = "table-tr";
+    defensetr.appendChild(defensetdl);
+    defensetr.appendChild(defensetdr);
+    table.appendChild(defensetr);
+  
+    const special_attacktr = document.createElement('tr');
+    const special_attacktdl = document.createElement('td');
+    const special_attacktdr = document.createElement('td');
+    special_attacktdl.innerText = "Special-attack";
+    special_attacktdl.className = "table-td left";
+    special_attacktdr.innerText = pokemon['special_attack'];
+    special_attacktdr.className = "table-td right";
+    special_attacktr.className = "table-tr";
+    special_attacktr.appendChild(special_attacktdl);
+    special_attacktr.appendChild(special_attacktdr);
+    table.appendChild(special_attacktr);
+
+    const special_defensetr = document.createElement('tr');
+    const special_defensetdl = document.createElement('td');
+    const special_defensetdr = document.createElement('td');
+    special_defensetdl.innerText = "Special-defense";
+    special_defensetdl.className = "table-td left";
+    special_defensetdr.innerText = pokemon['special_defense'];
+    special_defensetdr.className = "table-td right";
+    special_defensetr.className = "table-tr";
+    special_defensetr.appendChild(special_defensetdl);
+    special_defensetr.appendChild(special_defensetdr);
+
+    const speedtr = document.createElement('tr');
+    const speedtdl = document.createElement('td');
+    const speedtdr = document.createElement('td');
+    speedtdl.innerText = "Speed";
+    speedtdl.className = "table-td left";
+    speedtdr.innerText = pokemon.speed;
+    speedtdr.className = "table-td right";
+    speedtr.className = "table-tr";
+    speedtr.appendChild(speedtdl);
+    speedtr.appendChild(speedtdr);
+    table.appendChild(speedtr);
+    
+  } else {
+    detailContainer.innerText = 'No data found!';
+  }
---
+++ Js, Dom(Week5)/scripts.js
@@ -0,0 +1,206 @@
+const data = [
+    { 
+      id: '1',
+      name: 'Bulbasaur',
+      height: '7',
+      weight: '69',
+      types: ['grass', 'poison'],
+      'base-Experience': '64',
+      abilities: ['overgrow', 'chlorophyll'],
+      hp: '45',
+      attack: '49',
+      defense: '49',
+      'special-attack': '65',
+      'special-defense': '65',
+      speed: '45',
+      img: '../img/pokemon1.png',
+    },
+    {
+      id: '2',
+      name: 'Ivysaur',
+      height: '10',
+      weight: '130',
+      types: ['grass', 'poison'],
+      'base-Experience': '142',
+      abilities: ['overgrow', 'chlorophyll'],
+      hp: '60',
+      attack: '62',
+      defense: '63',
+      'special-attack': '80',
+      'special-defense': '80',
+      speed: '60',
+      img: '../img/pokemon2.png',
+    },
+    {
+      id: '3',
+      name: 'Venusaur',
+      height: '20',
+      weight: '1000',
+      types: ['grass', 'poison'],
+      'base-Experience': '236',
+      abilities: ['overgrow', 'chlorophyll'],
+      hp: '80',
+      attack: '82',
+      defense: '83',
+      'special-attack': '100',
+      'special-defense': '100',
+      speed: '80',
+      img: '../img/pokemon3.png',
+    },
+    {
+      id: '4',
+      name: 'Charmander',
+      height: '6',
+      weight: '85',
+      types: ['fire'],
+      'base-Experience': '62',
+      abilities: ['blaze', 'solar-power'],
+      hp: '39',
+      attack: '52',
+      defense: '43',
+      'special-attack': '60',
+      'special-defense': '50',
+      speed: '65',
+      img: '../img/pokemon4.png',
+    },
+    {
+      id: '5',
+      name: 'Charmeleon',
+      height: '11',
+      weight: '190',
+      types: ['fire'],
+      'base-Experience': '142',
+      abilities: ['blaze', 'solar-power'],
+      hp: '58',
+      attack: '64',
+      defense: '58',
+      'special-attack': '80',
+      'special-defense': '65',
+      speed: '80',
+      img: '../img/pokemon5.png',
+    },
+    {
+      id: '6',
+      name: 'Charizard',
+      height: '17',
+      weight: '905',
+      types: ['fire', 'flying'],
+      'base-Experience': '240',
+      abilities: ['blaze', 'solar-power'],
+      hp: '78',
+      attack: '84',
+      defense: '78',
+      'special-attack': '109',
+      'special-defense': '85',
+      speed: '100',
+      img: '../img/pokemon6.png',
+    },
+    {
+      id: '7',
+      name: 'Squirtle',
+      height: '5',
+      weight: '90',
+      types: ['water'],
+      'base-Experience': '63',
+      abilities: ['torrent', 'rain-dish'],
+      hp: '44',
+      attack: '48',
+      defense: '65',
+      'special-attack': '50',
+      'special-defense': '64',
+      speed: '43',
+      img: '../img/pokemon7.png',
+    },
+    {
+      id: '8',
+      name: 'Wartortle',
+      height: '10',
+      weight: '225',
+      types: ['water'],
+      'base-Experience': '142',
+      abilities: ['torrent', 'rain-dish'],
+      hp: '59',
+      attack: '63',
+      defense: '80',
+      'special-attack': '65',
+      'special-defense': '80',
+      speed: '58',
+      img: '../img/pokemon8.png',
+    },
+    {
+      id: '9',
+      name: 'Blastoise',
+      height: '16',
+      weight: '855',
+      types: ['water'],
+      'base-Experience': '239',
+      abilities: ['torrent', 'rain-dish'],
+      hp: '79',
+      attack: '83',
+      defense: '100',
+      'special-attack': '85',
+      'special-defense': '105',
+      speed: '78',
+      img: '../img/pokemon9.png',
+    },
+    {
+      id: '10',
+      name: 'Caterpie',
+      height: '3',
+      weight: '29',
+      types: ['bug'],
+      'base-Experience': '39',
+      abilities: ['shield-dust', 'run-away'],
+      hp: '45',
+      attack: '30',
+      defense: '35',
+      'special-attack': '20',
+      'special-defense': '20',
+      speed: '45',
+      img: '../img/pokemon10.png',
+    },
+  ]
+
+const container = document.getElementById('pokemon-list');
+container.className = 'container';
+
+data.map(data1 => {
+    const item = document.createElement('div');
+    item.className = 'item';
+
+    const img = document.createElement('img');
+    img.src = data1.img;
+    img.className = 'item-img';
+
+    const iteminfo = document.createElement('div');
+    iteminfo.className = 'item-info';
+    
+    const name = document.createElement('h2');
+    name.innerText = data1.name;
+    name.className = 'item-info item-h2';
+    
+    const height = document.createElement('p');
+    height.innerText = `height: ${data1.height}dm`;
+    height.className = 'item-info item-p';
+
+    const weight = document.createElement('p');
+    weight.innerText = `weight: ${data1.weight}hg`;
+    weight.className = 'item-info item-p';
+
+    const types = document.createElement('p');
+    types.innerText = `types: ${data1.types.join(', ')}`
+    types.className = 'item-info item-p';
+
+    iteminfo.appendChild(name);
+    iteminfo.appendChild(height);
+    iteminfo.appendChild(weight);
+    iteminfo.appendChild(types);
+
+    item.appendChild(img);
+    item.appendChild(iteminfo);
+    item.addEventListener('click',() => {
+        localStorage.setItem('selected', JSON.stringify(data1));
+        window.location.href = 'detail.html';
+    })
+    container.appendChild(item);
+});
